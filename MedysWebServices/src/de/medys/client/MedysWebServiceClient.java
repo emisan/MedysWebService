fb77f79f4ff3f93c30e07bfb63fdb8c7b1bc2bd9
hek <hek@hayris-imac.fritz.box>
2018-07-25 13:35:33 +0200

    neue clients

---
+++ MedysWebServices/src/de/medys/client/MedysWebServiceClient.java
@@ -0,0 +1,34 @@
+package de.medys.client;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
+import de.medys.service.connectivity.ServiceInterface;
+
+public class MedysWebServiceClient
+{
+	public static void main(String[] args) {
+		
+		try
+		{
+			URL url = new URL("http://localhost:7070/HelloWebService?wsdl");
+			
+			QName qname = new QName("http://service.medys.de", "HelloWebService");
+			
+			Service service = Service.create(url, qname);
+			
+			ServiceInterface si = service.getPort(ServiceInterface.class);
+			
+			System.out.println(si.getTimeAsString());
+			System.out.println(si.getUserName());
+		}
+		catch (MalformedURLException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
--- MedysWebServices/src/de/medys/service/HelloWebservice.java
+++ MedysWebServices/src/de/medys/service/HelloWebservice.java
@@ -7,7 +7,7 @@ import javax.jws.WebService;
 
 import de.medys.service.connectivity.ServiceInterface;
 
-@WebService(name="HellosService",
+@WebService(name="HelloWebService",
 			serviceName="HelloWebService",
 			portName="HelloWebServicePort",
 			targetNamespace="http://service.medys.de",
