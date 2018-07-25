a86c9a030927a133a2a4ba972e5f79baebf0857c
hek <hek@hayris-imac.fritz.box>
2018-07-25 15:45:23 +0200

    mit JSF trial

--- MedysWebServices/src/de/medys/client/MedysWebServiceClient.java
+++ MedysWebServices/src/de/medys/client/MedysWebServiceClient.java
@@ -3,33 +3,71 @@ package de.medys.client;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import javax.annotation.ManagedBean;
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 
 import de.medys.service.connectivity.ServiceInterface;
 
+@ManagedBean(value="medysWebServiceClient")
 public class MedysWebServiceClient
 {
-	public static void main(String[] args) {
-		
+	private Service service;
+	private ServiceInterface helloWebServiceInterface;
+	
+	public MedysWebServiceClient()
+	{
 		try
 		{
 			URL url = new URL("http://localhost:7070/medys/HelloWebService?wsdl");
 			
 			QName qname = new QName("http://service.medys.de", "HelloWebService");
 			
-			Service service = Service.create(url, qname);
+			service = Service.create(url, qname);
 			
-			ServiceInterface si = service.getPort(ServiceInterface.class);
-			
-			System.out.println(si.getTimeAsString());
-			System.out.println(si.getUserName());
-			System.out.println(si.sayHello());
+			helloWebServiceInterface = service.getPort(ServiceInterface.class);
 		}
 		catch (MalformedURLException e)
 		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public void setzeUsername(String username)
+	{
+		if(helloWebServiceInterface != null && username != null)
+		{
+			helloWebServiceInterface.setUserName(username);
+		}
+	}
+	
+	public String gibUsername()
+	{
+		return helloWebServiceInterface != null ? helloWebServiceInterface.getUserName() : "WebService Schnittstelle \"HelloWebService\" ist NULL!";
+	}
+	
+	public String sagHallo()
+	{
+		String s = null;
+		if(helloWebServiceInterface != null)
+		{
+			s = helloWebServiceInterface.sayHello();
+		}
+		return s;
+	}
+	
+	public String gibAktuelleZeit()
+	{
+		String zeit = null;
+		
+		if(helloWebServiceInterface != null)
+		{
+			zeit = helloWebServiceInterface.getTimeAsString();
+		}
+		return zeit;
+	}
+	public static void main(String[] args) {
+		new MedysWebServiceClient();
 	}
 }
