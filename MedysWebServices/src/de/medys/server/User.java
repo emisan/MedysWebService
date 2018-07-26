3b3958a077103b1c6b9c53151ae24af947f82c68
hek <hek@hayris-imac.fritz.box>
2018-07-26 11:30:42 +0200

    trial with jpa and myysql connectivity start

---
+++ MedysWebServices/src/de/medys/server/User.java
@@ -0,0 +1,89 @@
+package de.medys.server;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="t_users")
+public class User
+{
+	private Long id;
+	private String vorname;
+	private String nachname;
+	private LocalDate geburtstag;
+	
+	private List<Adresse> adressen;
+	
+	
+	public User()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	@Id
+	@GeneratedValue
+	public Long getId()
+	{
+		return id;
+	}
+
+	public void setId(Long id)
+	{
+		this.id = id;
+	}
+
+	@Column(name="vorname", nullable=false)
+	public String getVorname()
+	{
+		return vorname;
+	}
+
+	public void setVorname(String vorname)
+	{
+		this.vorname = vorname;
+	}
+
+	@Column(name="nachname", nullable=false)
+	public String getNachname()
+	{
+		return nachname;
+	}
+
+	public void setNachname(String nachname)
+	{
+		this.nachname = nachname;
+	}
+
+	@OneToMany(mappedBy="user", fetch=FetchType.LAZY)
+	public List<Adresse> getAdressen()
+	{
+		return adressen;
+	}
+
+	public void setAdressen(List<Adresse> adressen)
+	{
+		this.adressen = adressen;
+	}
+	
+	@Column(name="gebutrstag", nullable=false)
+	@Temporal(TemporalType.DATE)
+	public LocalDate getGeburtstag()
+	{
+		return geburtstag;
+	}
+
+	public void setGeburtstag(LocalDate geburtstag)
+	{
+		this.geburtstag = geburtstag;
+	}
+}
